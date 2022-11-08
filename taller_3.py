3addacb3e781c36170ba55d839ea519315b055ca
rarifbkn <rarifbkn@gmail.com>
2022-11-08 20:07:56 -0300

    login funcionando
    >:v

--- taller_3.py
+++ taller_3.py
@@ -7,8 +7,8 @@ def connection():
             host='localhost',
             database='taller_3',
             user='postgres',
-             password='pingui840', 
-             port='5432')
+            password='pingui840', 
+            port='5432')
         return connection
     except(Exception, Error) as error:
         connection.rollback()
@@ -40,62 +40,65 @@ def select_query(query,data=[]):
     except(Exception, Error) as error:
         print("Error: %s" % error)
 
-def verificar_query(query,data=[]):
-    try:
-        con = connection()
-        cursor = con.cursor()
-        if con and query != '' and data == []:
-            cursor.execute(query)
-        elif con and query != '' and data != []: #Con mas parametros 
-            cursor.execute(query,data) 
-        return True
-    except(Exception, Error) as error:
-        print("Error: %s" % error)
-
-print("========================================================")
-print("=========Bienvenido/a al Negocio de Juanita ===========")
+print("============================================================")
+print("=========Bienvenido/a al Negocio de Juanita :D==============")
 print()
 
-# creaciond de login
+# creacion de login
 try:
+    con = connection()
+    cursor = con.cursor()
     rut = str(input("Ingrese su rut: "))
-    if(rut.lower() != "admin"):
-        query = "SELECT COUNT(rut) FROM cliente GROUP BY HAVING rut = %s"
-        resultado = verificar_query(query,[rut])
-        if (resultado == 1): # el rut esta en la base de datos
+    if(rut.lower() != "admin"): # es un cliente
+        query = "SELECT rut FROM cliente WHERE rut = %s"
+        resultado = select_query(query,[rut])
+        if (resultado != []): # el rut esta en la base de datos
             password = str(input("Ingrese su contraseña: "))
-            query = "SELECT COUNT(rut) FROM cliente GROUP BY rut HAVING rut = %s AND passw = %s"
-            login = verificar_query(query,[rut,password])
-            while (login == 0) :
+            query = "SELECT rut FROM cliente WHERE rut = %s AND passw = %s"
+            login = select_query(query,[rut,password])
+            while (login == []) :
                 password = str(input("Error,Ingrese su contraseña nuevamente: "))
-                query = "SELECT COUNT(rut) FROM cliente GROUP BY rut HAVING rut = %s AND passw = %s"
-                login = verificar_query(query,[rut,password]) 
+                query = "SELECT rut FROM cliente WHERE rut = %s AND passw = %s"
+                login = select_query(query,[rut,password]) 
             print("Bienvenido/a al sistema")
-        else: # el rut no se encuentra en la base de datos
+
+        else: # si el rut no se encuentra en la base de datos
             print("El rut ingresado no existe o esta erroneo...")
             registro = str(input("Esta registrarado? (si/no):"))
-            if registro.lower() == "si": #se equivoco en el rut
+            if (registro.lower() == "si" ): #si se equivoco en el rut
                 rut = str(input("Ingrese su rut nuevamente: "))
-                query = "SELECT * FROM cliente WHERE rut = %s"
-                resultado = verificar_query(query,[rut])
-                while not resultado:
+                query = "SELECT rut FROM cliente WHERE rut = %s "
+                resultado = select_query(query,[rut])
+                while resultado == []: # si se sigue equivocando
                     rut = str(input("Error,Ingrese su rut nuevamente: "))
-                    query = "SELECT * FROM cliente WHERE rut = %s"
-                    resultado = verificar_query(query,[rut])
-            else:
+                    query = "SELECT rut FROM cliente WHERE rut = %s AND passw = %s"
+                    resultado = select_query(query,[rut])
+                password = str(input("Ingrese su contraseña: "))
+                query = "SELECT rut FROM cliente WHERE rut = %s AND passw = %s"
+                login = select_query(query,[rut,password])
+                while (login == []) :
+                    password = str(input("Error,Ingrese su contraseña nuevamente: "))
+                    query = "SELECT rut FROM cliente WHERE rut = %s AND passw = %s"
+                    login = select_query(query,[rut,password]) 
+                print("Bienvenido/a al sistema")    
+
+            else: # si no esta registrado
+                query = "SELECT SUM(cant_carritos) FROM (SELECT COUNT(carrito_id) AS cant_carritos FROM carrito GROUP BY carrito_id) AS tabla"
+                cant_carritos = select_query(query)
+                print (cant_carritos[0][0])
                 print("Ingrese sus datos para registrarse")
                 usuario = str(input("Ingrese su nombre: "))
-                rut = str(input("Ingrese su rut: "))
-                carrito_id = str(input("Ingrese numero de carrito: "))
+                carrito_id = cant_carritos[0][0] + 1
+                query = "INSERT INTO carrito (carrito_id,monto_total) VALUES (%s,0) "
+                insert_query(query,[carrito_id]) 
                 password = str(input("Ingrese su contraseña: "))
                 saldo = int(input("Ingrese su saldo: "))
                 query = "INSERT INTO cliente (usuario,rut,carrito_id,passw,saldo) VALUES (%s,%s,%s,%s,%s)"
                 insert_query(query,[usuario,rut,carrito_id,password,saldo])
                 print("Se ha registrado correctamente")
-
-    else:
+    else:# es admin
         password = str(input("Ingrese su contraseña: "))
-        while password.lower() != "negociojuanita":
+        while password.lower() != "negociojuanita": #se equivocó en la contraseña
             password = str(input("Error,Ingrese su contraseña nuevamente: "))
         print(".....Bienvenido/a al menu administrador....")
         print("1. Bloquear usuario")
